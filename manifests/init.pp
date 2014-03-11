2ab7472c336d642e1490c6a6c85d034cb0beac6d
beergeek <brett.gray@puppetlabs.com>
2014-03-11 21:35:11 +1100

    Added commenting

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,26 +6,20 @@
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# No parameters
 #
 # === Variables
 #
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+# No variables
 #
 # === Examples
 #
 #  class { mco_agents:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
 #  }
+#
+# or
+#
+# include mco_agents
 #
 # === Authors
 #
