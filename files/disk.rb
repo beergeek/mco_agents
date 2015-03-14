3183afd0ed28f0b2147ab4087594e2055c3dd6da
beergeek <brett.gray@puppetlabs.com>
2015-03-14 08:29:53 +0800

    Update disk.rb

--- files/disk.rb
+++ files/disk.rb
@@ -2,9 +2,7 @@ module MCollective
   module Agent
     class Disk < RPC::Agent
       action 'get_df' do
-        out = ''
-        run("df -h --exclude-type=nfs --exclude-type=nfsv4 --exclude-type=tmpfs | awk '{if(NR>1)print}'", :stdout => out, :stderr => :err, :chomp => true)
-        reply[:output] = out
+        reply[:statuscode] = run("df -h --exclude-type=nfs --exclude-type=nfsv4 --exclude-type=tmpfs | awk '{if(NR>1)print}'", :stdout => :out, :stderr => :err, :chomp => true)
       end
     end
   end
